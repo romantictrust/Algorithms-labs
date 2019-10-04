4647ed6b887483580d28a9f8f26db001c57894ed
sipep <vladcirkunov598@gmail.com>
2019-10-05 02:07:51 +0300

    Initialization

---
+++ Lab 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Global
+{
+    class Program
+    {
+        static void Main()
+        {
+            int arrLen = 1000;
+            int[] fromTo = { -1, 2 };
+            ArrayCreator arr = new ArrayCreator(arrLen, fromTo);
+            foreach (var entry in arr.Arr)
+            {
+                Console.Write("{0} ",entry);
+            }
+        }
+    }
+}
---
+++ Lab 1/Sorts.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Global
+{
+    public class Sorts
+    {
+        private int[] arr;
+
+        public int[] qSort(int[] arr){
+            return arr;
+        }
+    }
+}
