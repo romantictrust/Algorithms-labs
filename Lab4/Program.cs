863c07249df78ca4cbff9dafb5fa3b93fad963a8
romantictrust <vladcirkunov598@gmail.com>
2019-11-28 16:05:06 +0300

    Lab4: Init

---
+++ Lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = 0;
+            int to = (int)Math.Pow(10, 7);
+            int[] fromTo = { from, to };
+            int size = (int)Math.Pow(10, 7);
+
+            ArrayCreator objArr = new ArrayCreator(size, fromTo);
+            int[] arr = objArr.getArray;
+        }
+    }
+}
